cd20d1e7b956c7801fa2fc146e554604ae351e70
Adilek26 <62073785+Adilek26@users.noreply.github.com>
2020-06-14 12:03:58 -0500

    first commit

---
+++ src/test/java/com/Project/testbase/PageInitializer.java
@@ -0,0 +1,23 @@
+package com.Project.testbase;
+
+import com.Project.pages.DashboardPageElements;
+import com.Project.pages.LoginPageElements;
+import com.Project.pages.PersonalDetailsPageElements;
+
+//Initialize all pages classes and stores reference in static variables
+//that will called /used in test classes
+
+public class PageInitializer extends BaseClass{
+
+	public static LoginPageElements login;
+	public static PersonalDetailsPageElements pdetails;
+	public static DashboardPageElements dashboard;
+	
+	public static void initialize() {
+		
+		login=new LoginPageElements();
+		pdetails=new PersonalDetailsPageElements();
+		dashboard=new DashboardPageElements();
+	}
+	
+}
